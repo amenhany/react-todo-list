e0a48c264bb4ee1fdc7e0123d11f4e04de707747
Amen Hany <amen.hany.2810@gmail.com>
2025-02-03 17:06:48 +0200

    Form validation

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,36 +1,49 @@
-import { Add } from "@mui/icons-material";
-import { ListItem, Input, IconButton } from "@mui/joy";
+import { Add, InfoOutlined, SettingsPhoneRounded } from "@mui/icons-material";
+import { ListItem, Input, IconButton, FormControl, FormHelperText } from "@mui/joy";
 import { useState } from "react";
 
 export default function TodoForm({ add }) {
     const [text, setText] = useState("");
+    const [isFormValid, setIsFormValid] = useState(true);
 
     function handleChange(evt) {
+        if (evt.target.value !== "") setIsFormValid(true);
         setText(evt.target.value);
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-        add(text);
-        setText("");
+        if (text !== "") {
+            add(text);
+            setText("");
+        }
+        else setIsFormValid(false);
     }
 
     return (
         <ListItem>
             <form onSubmit={handleSubmit}>
-                <Input
-                    variant="outlined"
-                    color="primary"
-                    sx={{color: "var(--joy-palette-neutral-700, #32383E)"}}
-                    placeholder="Type something to do..."
-                    startDecorator={
-                        <IconButton type="submit" aria-label="Add" size="sm" color="primary">
-                            <Add />
-                        </IconButton>
+                <FormControl error={!isFormValid}>
+                    <Input
+                        variant="outlined"
+                        color="primary"
+                        sx={{color: "var(--joy-palette-neutral-700, #32383E)"}}
+                        placeholder="Type something to do..."
+                        startDecorator={
+                            <IconButton type="submit" aria-label="Add" size="sm" color="primary">
+                                <Add />
+                            </IconButton>
+                        }
+                        value={text}
+                        onChange={handleChange}
+                    />
+                    { !isFormValid &&
+                        <FormHelperText>
+                            <InfoOutlined />
+                            Can not add an empty Todo!
+                        </FormHelperText>
                     }
-                    value={text}
-                    onChange={handleChange}
-                />
+                </FormControl>
             </form>
         </ListItem>
     );
